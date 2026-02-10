30744f0c76b4d7198fa48eebfb514224775aa9c5
xden <mail@xden.dev>
2026-02-10 05:08:43 +0000

    fix: csrf missmatch

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,15 +3,47 @@ import App from "./App.vue";
 import axios from "axios";
 import '../css/artgui.css';
 
+// Отключаем автоматическую обработку XSRF
+axios.defaults.xsrfCookieName = null;
+axios.defaults.xsrfHeaderName = null;
+
+// Создаём экземпляр
+const apiClient = axios.create({
+    headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+        'Accept': 'application/json'
+    }
+});
+
+// Interceptor для добавления токена
+apiClient.interceptors.request.use(config => {
+    const token = document.querySelector('meta[name="csrf-token"]')?.getAttribute('content');
+
+    if (token) {
+        config.headers['X-CSRF-TOKEN'] = token;
+    }
+
+    console.log('Sending request with CSRF token:', token); // Для отладки
+
+    return config;
+});
+
+// Interceptor для ошибок
+apiClient.interceptors.response.use(
+    response => response,
+    error => {
+        console.error('Request failed:', error.response); // Для отладки
+
+        if (error.response?.status === 419) {
+            alert('CSRF token mismatch. Check console.');
+        }
+
+        return Promise.reject(error);
+    }
+);
+
 const app = createApp({});
-
-// Регистрация компонента
 app.component('app', App);
-
-// Глобальный axios (Vue 3 способ)
-app.config.globalProperties.$axios = axios;
-
-// Provide для Composition API
-app.provide('axios', axios);
-
+app.config.globalProperties.$axios = apiClient;
+app.provide('axios', apiClient);
 app.mount('#app');
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,22 +1,18 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Artisan ~ {{ $name = config('app.name', 'Laravel') }}</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('vendor/artgui/artgui.css') }}">
 </head>
 <body class="bg-gray-100 font-mono">
 
 <div id="app">
-    <app
-            home="{{ url('/') }}"
-            endpoint="{{ config('artgui.force-https', false) ? secure_url(URL::route('artgui.index', [], false)) : route('artgui.index') }}"
-            title="{{ config('artgui.title') }}"
-    />
+    <app home="{{ url('/') }}" endpoint="{{ route('artgui.index') }}" title="{{ config('artgui.title') }}"/>
 </div>
 
 <script defer src="{{ asset('vendor/artgui/artgui.js') }}"></script>
